8d388c356e86485adfac6f48074713fcf524d9e2
kalyanPalgutta <107052613+kalyanPalgutta@users.noreply.github.com>
2022-07-15 11:17:04 +0530

    Initial commit

---
+++ EmployeeApp/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+class Home(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+
+
+# Create your views here.
